a5c1ef7439691a1e2046e081b8a6edc7e8bbdc7d
Humberto Silveira <humberto_ss@hotmail.com>
2021-11-01 22:23:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Navbar />
+    <div className="container">
      <h1 className="text-primary">Hello</h1>
+     <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
